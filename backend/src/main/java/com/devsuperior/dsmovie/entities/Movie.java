193497d5df0209cc1f2c43bd1ea6772d0232bc2b
Dominic Lourenço Barbosa <mobarbosa.phoenix@gmail.com>
2022-03-13 19:40:31 -0300

    save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.NoArgsConstructor;
@@ -20,6 +24,9 @@ public class Movie {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	@OneToMany(mappedBy="id.movie")
+	private Set<Score> scores = new HashSet<>();
 	
 	public Movie(Long id, String title, Double score, Integer count, String image) {
 		this.id = id;
@@ -68,8 +75,9 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
-	
-	
+
+	public Set<Score> getScores() {
+		return scores;
+	}
 	
 }
--- backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -1,7 +1,14 @@
 package com.devsuperior.dsmovie.entities;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 @Entity
 @Table(name="tb_user")
 public class User {
